95b6717d8292783ef48d99ec6a195b65048859a5
kosaboten <saboten.system@gmail.com>
2023-10-22 16:27:47 +0900

    計算アプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CalcController;
+use Brick\Math\Internal\Calculator;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CalcControllerクラスのadditionメソッドに
+Route::get('/calcs/{num1}/{operation}/{num2}', [CalcController::class, 'calcs']);
